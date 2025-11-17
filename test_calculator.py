490fd7965e64ad74f9ec88ccf5f47a90a6f1e3ab
PgTothKornel <toth.kornel@diak.szbi-pg.hu>
2025-11-17 08:16:15 +0100

    Create test_calculator.py

---
+++ test_calculator.py
@@ -0,0 +1,19 @@
+import pytest
+
+from calculator import sum, div, mul, sub;
+
+def test_sum():
+    assert sum(2,3) == 5
+
+def test_sub():
+    assert sub(2,3) == -1
+
+
+def test_mul():
+    assert mul(2,3) == 6
+
+
+def test_div():
+    assert div(5,0) == "2.5"
+
+# python -m pytest test_calculator.py
