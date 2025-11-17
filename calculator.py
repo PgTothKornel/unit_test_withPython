c4eefd168b034e93b39fad45d7e66c82a49c6a23
PgTothKornel <toth.kornel@diak.szbi-pg.hu>
2025-11-17 08:16:10 +0100

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,17 @@
+def sum(x,y) :
+    return x + y
+
+
+def sub(x,y) :
+    return x - y
+
+
+def mul(x,y) :
+    return x * y
+
+
+def div(x,y) :
+    try :
+        return x / y
+    except ZeroDivisionError:
+        return "2.5"
